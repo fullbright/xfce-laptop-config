f671cd24d832818319efcfc5be0f206329fb98ee
fullbright <full3right@gmail.com>
2018-06-10 23:03:38 +0200

    Update install_tools.sh
    Add sudo command in front of the lines to give sudo privileges

--- install_tools.sh
+++ install_tools.sh
@@ -2,6 +2,8 @@
 
 CURRENT_USER=$(whoami)
 
+echo "Current user is $CURRENT_USER"
+
 echo "Installing base components"
 curl -s https://raw.githubusercontent.com/fullbright/historycrawler/master/install_base_tools.sh | sudo sh /dev/stdin $CURRENT_USER
 
@@ -15,39 +17,39 @@ sudo -u $CURRENT_USER git reset --hard origin/master
 
 echo "Installating custom applications"
 # Albert, Filezilla, vlc, audacity, Inkscape
-apt-get install albert vlc browser-plugin-vlc filezilla audacity inkscape -y
+sudo apt-get install albert vlc browser-plugin-vlc filezilla audacity inkscape -y
 
 # Google chrome, docker, Sublime text
 sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-apt-get update
-apt-get install google-chrome-stable -y
+sudo apt-get update
+sudo apt-get install google-chrome-stable -y
 
 # Docker
 
-apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
 
-apt-get install docker-ce -y
+sudo apt-get install docker-ce -y
 
-groupadd docker
-usermod -aG docker $USER
-systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
 
 # Sublime text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt-get update
-apt-get install sublime-text -y
+sudo apt-get update
+sudo apt-get install sublime-text -y
 
 # Arc theme
-sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
 wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
 sudo apt-key add - < Release.key
-apt-get update
-apt-get install arc-theme
+sudo apt-get update
+sudo apt-get install arc-theme
 rm Release.key
 
 
