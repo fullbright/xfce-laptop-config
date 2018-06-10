58c71866d7fe461d8b491a2a0cce45d23c89ab27
fullbright <full3right@gmail.com>
2018-06-10 23:19:58 +0200

    Update install_tools.sh
    Add sudo to Google Chrome installation

--- install_tools.sh
+++ install_tools.sh
@@ -20,7 +20,7 @@ echo "Installating custom applications"
 sudo apt-get install albert vlc browser-plugin-vlc filezilla audacity inkscape -y
 
 # Google chrome, docker, Sublime text
-sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install google-chrome-stable -y
