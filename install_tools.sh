b7dc95b38c734faefe94c68c3fa93cfb47e91dad
fullbright <full3right@gmail.com>
2018-06-10 21:05:33 +0200

    Update install_tools.sh
    Fix installation issue with -y flag

--- install_tools.sh
+++ install_tools.sh
@@ -20,7 +20,7 @@ apt-get install google-chrome-stable -y
 
 # Docker
 
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
